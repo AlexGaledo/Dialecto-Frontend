b41ea1397d132c02973700dd7743f852575fd814
AlexGaledo <alexgaledo@gmail.com>
2025-04-25 16:53:00 +0800

    Update Microphone.jsx

--- frontend/src/pages/Microphone.jsx
+++ frontend/src/pages/Microphone.jsx
@@ -28,7 +28,7 @@ export default function Microphone() {
                 setResponse(`You said: ${recognizedText}`);
                 
                 try {
-                    const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+                    const API_URL = import.meta.env.VITE_API_URL;
                     const res = await fetch(`${API_URL}/chatbot`,{
                         method: "POST",
                         headers: {
